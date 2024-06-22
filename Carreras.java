581561e1b1d0964643fd917d966089adc2477923
Antuuf <98172181+Antuuf@users.noreply.github.com>
2024-06-22 17:04:45 -0300

    Create Carreras.java

---
+++ Carreras.java
@@ -0,0 +1,34 @@
+package aed;
+
+import java.util.List;
+
+public class Carreras {
+    private String nombre;
+    private Trie<Materia> materias;
+
+    public Carreras(String nombre) {
+        this.nombre = nombre;
+        this.materias = new Trie<>();
+    }
+
+    public void agregarMateria(String nombreMateria, ParCarreraMateria[] info) {
+        Materia nuevaMateria = new Materia(info);
+        materias.insertar(nombreMateria, nuevaMateria);
+    }
+
+    public void eliminarMateria(String nombreMateria) {
+        materias.borrar(nombreMateria);
+    }
+    
+    public Materia obtenerMateria(String nombreMateria) {
+        return materias.obtenerDef(nombreMateria);
+    }
+
+    public List<String> obtenerTodasLasMaterias() {
+        return materias.obtenerClaves();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
