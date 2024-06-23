2069fef3b2388447f5b1397e38c7ce2e5da3b618
Antuuf <98172181+Antuuf@users.noreply.github.com>
2024-06-23 12:33:18 -0300

    Update SistemaSIU.java

--- SistemaSIU.java
+++ SistemaSIU.java
@@ -6,166 +6,122 @@ public class SistemaSIU {
     private Trie<Carreras> carreras;
     private Trie<Estudiantes> estudiantes;
 
+    enum CargoDocente {
+        AY2, AY1, JTP, PROF
+    }
+
+    // constructor
     public SistemaSIU() {
         this.carreras = new Trie<>();
         this.estudiantes = new Trie<>();
-    }
+    } // Complejidad: O(1)
 
-    public SistemaSIU(InfoMateria[] infoMaterias, String[] nombresCarreras) {
+    // constructor con InfoMateria y libretas universitarias
+    public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias) {
         this();
-        for (String nombreCarrera : nombresCarreras) {
-            agregarCarrera(nombreCarrera);
-        }
-    }
-
-    // Agregar una carrera al sistema
-    public void agregarCarrera(String nombreCarrera) {
-        Carreras nuevaCarrera = new Carreras(nombreCarrera);
-        carreras.insertar(nombreCarrera, nuevaCarrera);
-    }
-
-    public List<String> obtenerTodasLasCarreras() {
-        List<String> listaCarreras = carreras.obtenerClaves();
-        listaCarreras.sort(String::compareTo); // Orden lexicográfico
-        return listaCarreras;
-    }
-
-    // Agregar un estudiante al sistema
-    public void agregarEstudiante(String LU) {
-        Estudiantes nuevoEstudiante = new Estudiantes();
-        estudiantes.insertar(LU, nuevoEstudiante);
-    }
-
-    public void inscribir(String LU, String nombreCarrera, String nombreMateria) {
-        Estudiantes estudiante = estudiantes.obtenerDef(LU);
-        Carreras carrera = carreras.obtenerDef(nombreCarrera);
-    
-        if (estudiante != null && carrera != null) {
-            ParCarreraMateria[] info = { new ParCarreraMateria(nombreCarrera, nombreMateria) };
-            Materia materia = carrera.obtenerMateria(nombreMateria);
-    
-            if (materia == null) {
-                carrera.agregarMateria(nombreMateria, info);
-                materia = carrera.obtenerMateria(nombreMateria);
-            }
-    
-            estudiante.inscribirMateria(nombreMateria, info);
-            materia.agregarAlumno(LU);
-    
-            // Inscribir en las equivalencias de otras carreras
-            for (ParCarreraMateria par : materia.obtenerNombres()) {
-                Carreras otraCarrera = carreras.obtenerDef(par.getCarrera());
-                if (otraCarrera != null) {
-                    Materia otraMateria = otraCarrera.obtenerMateria(par.getNombreMateria());
-                    if (otraMateria != null) {
-                        estudiante.inscribirMateria(par.getNombreMateria(), info);
-                        otraMateria.agregarAlumno(LU);
-                    }
+        for (InfoMateria info : infoMaterias) {
+            for (ParCarreraMateria par : info.getParesCarreraMateria()) {
+                Carreras carrera = carreras.obtenerDef(par.getCarrera());
+                if (carrera == null) {
+                    carrera = new Carreras(par.getCarrera());
+                    carreras.insertar(par.getCarrera(), carrera);
+                }
+                Materia materia = carrera.obtenerMateria(par.getNombreMateria());
+                if (materia == null) {
+                    carrera.agregarMateria(par.getNombreMateria(), new ParCarreraMateria[]{par});
                 }
             }
         }
-    }
-    
-    
+        for (String lu : libretasUniversitarias) {
+            Estudiantes estudiante = new Estudiantes();
+            estudiantes.insertar(lu, estudiante);
+        }
+    } // Complejidad: O(Pc * |c| * |Mc| + Pm * |Nm| + E)
 
-    public void agregarDocente(CargoDocente cargo, String nombreCarrera, String nombreMateria) {
-        Carreras carrera = carreras.obtenerDef(nombreCarrera);
-        if (carrera != null) {
-            Materia materia = carrera.obtenerMateria(nombreMateria);
-            if (materia != null) {
-                String tipoDocente = cargo.name();
-                materia.agregarDocente(tipoDocente);
-    
-                // Agregar docente a las equivalencias de otras carreras
-                ParCarreraMateria[] equivalencias = materia.obtenerNombres();
-                if (equivalencias != null) {
-                    for (ParCarreraMateria par : equivalencias) {
-                        Carreras otraCarrera = carreras.obtenerDef(par.getCarrera());
-                        if (otraCarrera != null) {
-                            Materia otraMateria = otraCarrera.obtenerMateria(par.getNombreMateria());
-                            if (otraMateria != null) {
-                                otraMateria.agregarDocente(tipoDocente);
-                            }
-                        }
-                    }
+    public void inscribir(String estudiante, String carrera, String materia) { // inscribe al estudiante en la materia `nombreMateria` de la carrera `carrera`.
+        Estudiantes est = estudiantes.obtenerDef(estudiante);
+        if (est != null) {
+            Carreras car = carreras.obtenerDef(carrera);
+            if (car != null) {
+                Materia mat = car.obtenerMateria(materia);
+                if (mat != null) {
+                    mat.agregarAlumno(estudiante);
+                    est.inscribirMateria(materia, new ParCarreraMateria[]{new ParCarreraMateria(carrera, materia)});
                 }
             }
         }
-    }
-    
+    } // Complejidad: O(|c| + |Mc|)
 
-    public int[] plantelDocente(String nombreCarrera, String nombreMateria) {
-        Carreras carrera = carreras.obtenerDef(nombreCarrera);
-        if (carrera != null) {
-            Materia materia = carrera.obtenerMateria(nombreMateria);
-            if (materia != null) {
-                return materia.obtenerDocentes();
+    public void agregarDocente(CargoDocente cargo, String carrera, String materia) {// agrega un docente con el cargo `cargo` a la materia `nombreMateria` de la carrera `carrera`.
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            Materia mat = car.obtenerMateria(materia);
+            if (mat != null) {
+                mat.agregarDocente(cargo.toString());
             }
         }
-        return new int[4];
-    }
+    }// Complejidad: O(|c| + |Mc|)
 
-    public int inscriptos(String nombreCarrera, String nombreMateria) {
-        int totalInscriptos = 0;
-        Carreras carrera = carreras.obtenerDef(nombreCarrera);
-    
-        if (carrera != null) {
-            Materia materia = carrera.obtenerMateria(nombreMateria);
-            if (materia != null) {
-                totalInscriptos += materia.obtenerCantidadAlumnos();
-    
-                // Contar inscriptos en las equivalencias de otras carreras
-                ParCarreraMateria[] otrasMaterias = materia.obtenerNombres();
-                if (otrasMaterias != null) {
-                    for (ParCarreraMateria par : otrasMaterias) {
-                        Carreras otraCarrera = carreras.obtenerDef(par.getCarrera());
-                        if (otraCarrera != null) {
-                            Materia otraMateria = otraCarrera.obtenerMateria(par.getNombreMateria());
-                            if (otraMateria != null) {
-                                totalInscriptos += otraMateria.obtenerCantidadAlumnos();
-                            }
-                        }
-                    }
-                }
+    public int[] plantelDocente(String materia, String carrera) { // devuelve un arreglo con la cantidad de docentes de cada tipo para la materia `nombreMateria` de la carrera `carrera`.
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            Materia mat = car.obtenerMateria(materia);
+            if (mat != null) {
+                return mat.obtenerDocentes();
             }
         }
-    
-        return totalInscriptos;
-    }
-    
+        return new int[]{0, 0, 0, 0};
+    } // Complejidad: O(|c| + |Mc|)
 
-    // Verificar si una materia excede el cupo permitido
-    public boolean excedeCupo(String nombreCarrera, String nombreMateria) {
-        Carreras carrera = carreras.obtenerDef(nombreCarrera);
-        if (carrera != null) {
-            Materia materia = carrera.obtenerMateria(nombreMateria);
-            if (materia != null) {
-                return materia.excedeCupo();
+    public void cerrarMateria(String materia, String carrera) { // Cierra la materia `nombreMateria` en todas las carreras por falta o exceso de docentes.
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            car.eliminarMateria(materia);
+        }
+    } // Complejidad: O(|c| + |Mc| + |Nm| + Em)
+
+    public int inscriptos(String materia, String carrera) { // devuelve la cantidad de estudiantes inscriptos en la materia `nombreMateria` de la carrera `carrera`.
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            Materia mat = car.obtenerMateria(materia);
+            if (mat != null) {
+                return mat.obtenerCantidadAlumnos();
+            }
+        }
+        return 0;
+    } // Complejidad: O(|c| + |Mc|)
+
+    public boolean excedeCupo(String materia, String carrera) { // indica si la cantidad de inscriptos en la materia `nombreMateria` de la carrera `carrera` excede el cupo.
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            Materia mat = car.obtenerMateria(materia);
+            if (mat != null) {
+                return mat.excedeCupo();
             }
         }
         return false;
-    }
+    } // Complejidad: O(|c| + |Mc|)
 
-    // Obtener lista de todas las materias de un estudiante
-    public List<String> materiasInscriptas(String LU) {
-        Estudiantes estudiante = estudiantes.obtenerDef(LU);
-        if (estudiante != null) {
-            return estudiante.materiasInscriptas();
+    public String[] carreras() { // devuelve un arreglo ordenado alfabéticamente con todas las carreras del sistema.
+        List<String> listaCarreras = carreras.obtenerClaves(); 
+        return listaCarreras.toArray(new String[0]);
+    } // Complejidad: O(Pc * |c|)
+
+    public String[] materias(String carrera) { // devuelve un arreglo ordenado alfabéticamente con todas las materias de la carrera `carrera`.
+        // Complejidad: O(|c| + Pmc * |mc|)
+        Carreras car = carreras.obtenerDef(carrera);
+        if (car != null) {
+            List<String> listaMaterias = car.obtenerTodasLasMaterias();
+            return listaMaterias.toArray(new String[0]);
         }
-        return null;
-    }
+        return new String[0];
+    } // Complejidad: O(|c| + Pmc * |mc|)
 
-    // Eliminar un estudiante del sistema
-    public void eliminarEstudiante(String LU) {
-        estudiantes.borrar(LU);
-    }
-
-    // Método para obtener la lista de carreras
-    public List<String> carreras() {
-        return obtenerTodasLasCarreras();
-    }
-
-    public enum CargoDocente {
-        AY2, AY1, JTP, PROF
-    }
+    public int materiasInscriptas(String estudiante) { // devuelve la cantidad de materias en las que está inscrito el estudiante `estudiante`.
+        Estudiantes est = estudiantes.obtenerDef(estudiante);
+        if (est != null) {
+            return est.materiasInscriptas().size();
+        }
+        return 0;
+    }// Complejidad: O(1)
 }
